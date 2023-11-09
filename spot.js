1ac24ea1b158b2b23f79470cf9ba1c700546dd6d
KramesDt <lordkratos365@gmail.com>
2023-11-09 06:18:08 +0100

    feature: spot trade delete order and all orders

--- spot.js
+++ spot.js
@@ -68,13 +68,89 @@ async function makeTrade(symbol, price, action, quantity) {
 }
 
 async function deleteSpotTrade(symbol, orderId){
-    const apiKey = process.env.BINANCE_API_KEY;
-    const apiSecret = process.env.BINANCE_API_SECRET;
-    const endpoint = `https://api.binance.com/api/v3/sor/order/`;
-    const timestamp = Date.now();
+    try {
+      const apiKey = process.env.BINANCE_API_KEY;
+      const apiSecret = process.env.BINANCE_API_SECRET;
+      const endpoint = `https://api.binance.com/api/v3/order`;
+      const timestamp = Date.now();
+
+      const params = {
+        symbol,
+        orderId,
+        timestamp,
+        recvWindow: 5000,
+      };
+
+      let queryString = Object.keys(params)
+        .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+        .join("&");
+      const signature = crypto
+        .createHmac("sha256", apiSecret)
+        .update(queryString)
+        .digest("hex");
+
+      queryString += "&signature=" + signature;
+      const url = endpoint + "?" + queryString;
+
+      //
+      const request = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "X-MBX-APIHEY": apiKey,
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      });
+      console.log(url);
+
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      console.log("Error", error);
+      throw error;
+    }
 }
 
+async function deleteAllSpotTrade(symbol) {
+  try {
+    const apiKey = process.env.BINANCE_API_KEY;
+    const apiSecret = process.env.BINANCE_API_SECRET;
+    const endpoint = `https://api.binance.com/api/v3/cancelOrders`;
+    const timestamp = Date.now();
 
+    const params = {
+      symbol,
+      timestamp,
+      recvWindow: 5000,
+    };
+
+    let queryString = Object.keys(params)
+      .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+      .join("&");
+    const signature = crypto
+      .createHmac("sha256", apiSecret)
+      .update(queryString)
+      .digest("hex");
+
+    queryString += "&signature=" + signature;
+    const url = endpoint + "?" + queryString;
+
+    //
+    const request = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-MBX-APIHEY": apiKey,
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    console.log(url);
+
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log("Error", error);
+    throw error;
+  }
+}
 
 (async () => {
   const symbol = "MBLUSDT";
