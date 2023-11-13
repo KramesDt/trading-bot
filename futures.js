61f09a76f5825ef1bce29164a5a32f6a6e2c6519
KramesDt <lordkratos365@gmail.com>
2023-11-13 23:18:39 +0100

    feature: set min order notional

--- futures.js
+++ futures.js
@@ -13,15 +13,15 @@ async function getExchangeInfo(symbol) {
   }
 
   const symbolInfo = exchangeInfo.symbols.find((s) => s.symbol === symbol);
-  // console.log(symbolInfo)
+  console.log(symbolInfo)
 
   if (!symbolInfo) {
     throw new Error(`Symbol ${symbol} not found`);
   }
 
   const minOrderQty = symbolInfo.filters.find(
-    (f) => f.filterType === "LOT_SIZE"
-  ).minQty;
+    (f) => f.filterType === "MIN_NOTIONAL"
+  ).notional;
   return minOrderQty;
 }
 
@@ -126,27 +126,27 @@ async function deleteAllFuturesOrder(symbol) {
 (async () => {
   const symbol = "LQTYUSDT";
 
-  const balance = await checkFuturesBalance(symbol);
+  // const balance = await checkFuturesBalance(symbol);
 
   const leverage = 20; //Leverage on the futures
   const percent = 50; //Specify the percentage of USDT balance you want to trade with
-  const stake = parseFloat((percent / 100) * balance); //amount traded with in USDT
+  // const stake = parseFloat((percent / 100) * balance); //amount traded with in USDT
   const type = "LIMIT";
   const price = 1.3; //THe price you wanna trade at
   const action = "BUY";
   const orderId = 1206851765;
-  const quantity = Math.round(stake / price); //Quantity of assets futures traded
+  // const quantity = Math.round(stake / price); //Quantity of assets futures traded
 
-  // const orderQty = await getExchangeInfo(symbol);
+  const orderQty = await getExchangeInfo(symbol);
   // const transaction = await futuresOrder(symbol,leverage, action, quantity, price);
-  const openOrders = await getAllOpenFuturesOrders(); 
+  // const openOrders = await getAllOpenFuturesOrders(); 
   // const getAllOrders = await getAllFuturesOrders();
   // const deleteTransaction = await deleteFuturesOrder(symbol, orderId);
   // const deleteAllTransactions = await deleteAllFuturesOrder(symbol);
 
-  // console.log("Minimun Order Qty of", symbol, "is:", orderQty);
+  console.log("Minimun Order Notional Qty of", symbol, "is:", orderQty);
   // console.log("Futures Order:", transaction);
-  console.log("OPen Orders: ", openOrders);
+  // console.log("OPen Orders: ", openOrders);
   // console.log("All Orders: ", getAllOrders);
   // console.log("Balance is:", balance);
  // console.log("Successfully Deleted: ", deleteTransaction);
