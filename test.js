a33b7c945ecd59b273b45d7df52925e7dbf34de1
KramesDt <lordkratos365@gmail.com>
2023-12-08 19:40:29 +0100

    feature: trade on alert

---
+++ test.js
@@ -0,0 +1,30 @@
+const alertText1 =
+  "STIC Tool Alert: Buy potential: BINANCE:SOLUSD, Timeframe = 5 Book a buy position (usable on all timeframe) price = 62.72, volume = 1762.81. You should possible wait for confirmation close above the flip line (yellow line) if you are late. Do not FOMO if you are late. cryptosmartanalyst.com";
+const alertText2 =
+  "STIC Tool Alert: Buy potential: BINANCE:SOLUSD, Timeframe = 5 Book a buy position (usable on all timeframe) price = 62.72, volume = 3602.27. You should possible wait for confirmation close above the flip line (yellow line) if you are late. Do not FOMO if you are late. cryptosmartanalyst.com";
+
+// Function to extract details from the text
+const extractDetails = (text) => {
+  const details = {};
+
+  // Extracting information using regular expressions
+  const match = text.match(
+    /Buy potential: (\S+), Timeframe = (\d+) Book a buy position .* price = (\d+\.\d+), volume = (\d+\.\d+)/
+  );
+
+  if (match) {
+    details.symbol = match[1];
+    details.buyPotential = "Buy potential";
+    details.timeframe = match[2];
+    details.price = match[3];
+    details.volume = match[4];
+  }
+
+  return details;
+};
+
+// Save details into an array
+const alerts = [extractDetails(alertText1), extractDetails(alertText2)];
+
+// Display the result
+console.log(alerts);
